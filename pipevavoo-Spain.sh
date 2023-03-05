465f431d898695d0c28d68832a5cb28ea21f790e
zulu8888 <111640191+zulu8888@users.noreply.github.com>
2023-03-05 10:15:43 +0100

    Add files via upload

---
+++ pipevavoo-Spain.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+url="http://www2.vavoo.to/live2/index"
+wget -q --no-check-certificate $url 
+cat index | sed "s#http://#pipe:///home/ffpipe.sh http://#g" > index.m3u
+for country in Spain; do
+cat index.m3u | grep -E -A1 =\"$country > $country.m3u
+
+#m3u
+echo "#EXTM3U" > /home/pipevavoo-$country.m3u
+cat $country.m3u  >> /home/pipevavoo-$country.m3u
+rm -rf $country.m3u
+done
+rm -rf index.m3u
+rm index*
+exit 0
+
+
