d33ca6b8ac34c277300363962e3e6309a62b6446
zulu8888 <111640191+zulu8888@users.noreply.github.com>
2023-08-20 11:36:37 +0200

    pipevavoo-Spain.sh

--- pipevavoo-Spain.sh
+++ pipevavoo-Spain.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+url="http://www2.vavoo.to/live2/index"
 curl -k "https://www2.vavoo.to/live2/index?countries=all&output=json" > vavoo
 echo "#EXTM3U" > index
-cat vavoo | sed 's/\(}\),/\1}\n,/g' | sed 's/"url":"/"url":\n/g' | sed 's#,{\"group\":#\#EXTINF:-1 group-title=#g' | sed 's#,\"logo\":\"\",\"name\":#,#g' | sed 's/\"}.//' | sed 's/\",\"tvg.//' | sed 's#\",\"#\",#g' >> index
-mv index index.m3u
-cat index.m3u | sed "s#http://#pipe:///volume1/@appdata/tvheadend/ffpipe.sh http://#g" > index2.m3u
+cat vavoo | sed 's/\(}\),/\1}\n,/g' | sed 's/"url":"/"url":\n/g' | sed 's#,{\"group\":#\#EXTINF:-1 group-title=#g' | sed 's#,\"logo\":\"\",\"name\":#,#g' | sed 's/\"}.*//' | sed 's/\",\"tvg.*//' | sed 's#\",\"#\",#g'  >> index 
+cat index | sed "s#https://#pipe:///home/ffpipe.sh https://#g" > index.m3u
 for country in Spain; do
-cat index2.m3u | grep -E -A1 =\"$country > $country.m3u
+cat index.m3u | grep -E -A1 =\"$country > $country.m3u
 #m3u
-echo "#EXTM3U" > /volume1/web/vavoo-$country.m3u
-cat $country.m3u >> /volume1/web/vavoo-$country.m3u
+echo "#EXTM3U" > /home/pipevavoo-$country.m3u
+cat $country.m3u  >> /home/pipevavoo-$country.m3u
+rm -rf $country.m3u
 done
+rm -rf index.m3u
 rm index*
-rm vavoo
 exit 0
