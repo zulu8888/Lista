a91edc7cb8b772d3655381f9ca0dcf48b4e9874e
zulu8888 <111640191+zulu8888@users.noreply.github.com>
2023-08-20 20:36:05 +0200

    pipevavoo.sh

--- pipevavoo.sh
+++ pipevavoo.sh
@@ -4,7 +4,7 @@ curl -k "https://www2.vavoo.to/live2/index?countries=all&output=json" > vavoo
 echo "#EXTM3U" > index
 cat vavoo | sed 's/\(}\),/\1}\n,/g' | sed 's/"url":"/"url":\n/g' | sed 's#,{\"group\":#\#EXTINF:-1 group-title=#g' | sed 's#,\"logo\":\"\",\"name\":#,#g' | sed 's/\"}.*//' | sed 's/\",\"tvg.*//' | sed 's#\",\"#\",#g'  >> index 
 cat index | sed "s#https://#pipe:///home/ffpipe.sh https://#g" > index.m3u
-for country in Portugal Spain; do
+for country in Germany Turkey Albania Arabia Balkans Bulgaria France Hungary Italy Netherlands Poland Portugal Romania Russia Spain United Latin; do
 cat index.m3u | grep -E -A1 =\"$country > $country.m3u
 #m3u
 echo "#EXTM3U" > /home/pipevavoo-$country.m3u
