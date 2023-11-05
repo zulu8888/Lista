f2ef8bfff493ba7b5c929241b6c39e7893a142e7
zulu8888 <111640191+zulu8888@users.noreply.github.com>
2023-11-05 15:14:47 +0100

    Delete authkey.sh

--- authkey.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-vec=$(shuf -n 1 /home/vavookeys)
-
-curl -k --location --request POST 'https://www.vavoo.tv/api/box/ping2' --header 'Content-Type: application/json' --data "{\"vec\": \"$vec\"}" | sed 's#^.*"signed":"##' | sed "s#\"}}##g" | sed 's/".*//' > /home/authkey
-exit 0
